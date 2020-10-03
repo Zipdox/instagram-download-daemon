e7dcb4c96c9a4bfbcc49d8353218850e9578f435
Zipdox <42157341+Zipdox@users.noreply.github.com>
2020-10-03 19:50:09 +0200

    longer story cron interval

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var refreshHighlights;
         ]);
         console.log(await ig.challenge.sendSecurityCode(code));
     }).catch(e => console.log('Could not resolve checkpoint:', e, e.stack)).then(async () => {
-        refreshStories = schedule.scheduleJob('50 * * * *', checkStories);
+        refreshStories = schedule.scheduleJob('50 */6 * * *', checkStories);
         refreshPosts = schedule.scheduleJob('00 22 * * *', checkPosts);
         refreshHighlights = schedule.scheduleJob('00 20 * * *', checkHighlights);
     });
